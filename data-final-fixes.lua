e28dc65d257b8f1c2d5d15457b7882fe31987e56
Fernando <14892931+FMR7@users.noreply.github.com>
2026-05-05 14:59:15 +0200

    Fix max_speed error in robot speed settings

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,9 +1,17 @@
 local speed = settings.startup["faster-robots-speed"].value
 
 for _, bot in pairs(data.raw["logistic-robot"]) do
-    bot.speed = speed
+    if type(bot.max_speed) == "number" and speed > bot.max_speed then
+        bot.speed = bot.max_speed
+    else
+        bot.speed = speed
+    end
 end
 
 for _, bot in pairs(data.raw["construction-robot"]) do
-    bot.speed = speed
+    if type(bot.max_speed) == "number" and speed > bot.max_speed then
+        bot.speed = bot.max_speed
+    else
+        bot.speed = speed
+    end
 end
